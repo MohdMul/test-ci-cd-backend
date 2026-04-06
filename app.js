6ebb926d996987b2c76fd8b89c4b7a7a52e6d20b
Mohdniyam <niyamul.haque98@gmail.com>
2026-04-06 15:49:45 +0530

    Update response message in server to include user name

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from Docker Node App 🚀');
+  res.end('Hello from Docker Node App 🚀\n niyamul');
 });
 
 server.listen(3000, () => {
